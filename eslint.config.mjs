292d08912c1bdf654872a904a87b8fed8054d1a0
cavez86 <cavez86@gmail.com>
2025-05-08 23:54:41 +0200

    fix: add src/payload-types.ts to ignored files in eslint and prettier configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['node_modules', 'build', 'coverage', '.next'],
+    ignores: ['node_modules', 'build', 'coverage', '.next', 'src/payload-types.ts'],
   },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   prettier,
