f3750914af1e680ead1a2734de148cd083c75fa5
cavez86 <cavez86@gmail.com>
2025-05-29 23:51:47 +0200

    fix: update database connection string to use DATABASE_URL environment variable

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -53,7 +53,7 @@ export default buildConfig({
   },
   db: vercelPostgresAdapter({
     pool: {
-      connectionString: process.env.POSTGRES_URL || '',
+      connectionString: process.env.DATABASE_URL,
     },
   }),
   sharp,
