9f58f3baf2655da86c1f399c5f9b49cbf6338e41
cavez86 <cavez86@gmail.com>
2025-11-06 20:10:02 +0100

    fix: correct import paths for translation files in i18n configuration

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import enTranslations from '../messages/en.json';
-import itTranslations from '../messages/it.json';
+import enTranslations from '../../messages/en.json';
+import itTranslations from '../../messages/it.json';
 
 const resources = {
   en: {
