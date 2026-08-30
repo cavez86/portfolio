58d87469990d7452a5dcd31540318f4f9fc2d20a
cavez86 <cavez86@gmail.com>
2026-08-30 17:07:44 +0200

    chore: add biome ignore comment for non-null assertion in PostHog initialization

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,5 +1,6 @@
 import posthog from 'posthog-js';
 
+// biome-ignore lint/style/noNonNullAssertion: this value is guaranteed to be set in the environment
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: '/ingest',
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
